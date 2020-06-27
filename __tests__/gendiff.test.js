6828d59bb4f8855ab66d8d57986490f513a1323e
Alikhan Askarov <aaskarov@kaptechnology.kazatomprom.kz>
2020-06-28 02:08:11 +0600

    WIP

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,12 @@
 import { test, expect } from '@jest/globals';
+import fs from 'fs';
+import path from 'path';
 import gendiff from '../src/gendiff.js';
 
 test('test', () => {
   const beforePath = './__tests__/__fixtures__/before.json';
   const afterPath = './__tests__/__fixtures__/after.json';
-  const result = `{ host: hexlet.io, + timeout: 50, - timeout: 20, - proxy: 123.234.53.22, - follow: false, + verbose: true }`
-
-  expect(gendiff(beforePath, afterPath)).toBe(result);
+  const dirname = path.resolve();
+  const res = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/result.txt'), 'utf-8');
+  expect(gendiff(beforePath, afterPath)).toBe(res);
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -2,30 +2,30 @@ import fs from 'fs';
 import path from 'path';
 import has from 'lodash/has';
 
+const prettifyResultList = (list) => list.map((element, index) => (index === 0 ? element : ` ${element}`));
+
 export default (fileA, fileB) => {
   const dirname = path.resolve();
   const fileAJson = JSON.parse(fs.readFileSync(path.join(dirname, fileA), 'utf-8'));
   const fileBJson = JSON.parse(fs.readFileSync(path.join(dirname, fileB), 'utf-8'));
   const fileAKeys = Object.keys(fileAJson);
   const fileBKeys = Object.keys(fileBJson);
-  const removedAcc = fileAKeys.reduce((acc, key) => {
-    return has(fileBJson, key) ? acc : [...acc, `- ${key}: ${fileAJson[key]}`];
-  }, []);
-  console.log(removedAcc);
-  const addedAcc = fileBKeys.reduce((acc, key) => {
-    return has(fileAJson, key) ? acc : [...acc, `+ ${key}: ${fileBJson[key]}`];
-  }, []);
-  console.log(addedAcc);
+
+  const removedAcc = fileAKeys.reduce((acc, key) => (has(fileBJson, key) ? acc : [...acc, `- ${key}: ${fileAJson[key]}`]), []);
+  const addedAcc = fileBKeys.reduce((acc, key) => (has(fileAJson, key) ? acc : [...acc, `+ ${key}: ${fileBJson[key]}`]), []);
   const changedAcc = fileAKeys.reduce((acc, key) => {
     if (has(fileBJson, key) && fileAJson[key] !== fileBJson[key]) {
-      return [...acc, ` + ${key}: ${fileAJson[key]}`, ` - ${key}: ${fileBJson[key]}`]
-    } else if (has(fileBJson, key) && fileAJson[key] === fileBJson[key]) {
+      return [...acc, `+ ${key}: ${fileAJson[key]}`, `- ${key}: ${fileBJson[key]}`];
+    }
+    if (has(fileBJson, key) && fileAJson[key] === fileBJson[key]) {
       return [...acc, `${key}: ${fileAJson[key]}`];
     }
     return acc;
   }, []);
-  console.log(changedAcc)
-  const resultString = `{ ${[...changedAcc]}, ${[...removedAcc]}, ${[...addedAcc]} }`;
-  console.log(JSON.stringify(resultString))
+
+  const removedString = prettifyResultList(removedAcc);
+  const addedString = prettifyResultList(addedAcc);
+  const changedString = prettifyResultList(changedAcc);
+  const resultString = `{ ${[...changedString]} ${[...removedString]} ${[...addedString]} }`.split(', ').join(' ');
   return resultString;
 };
