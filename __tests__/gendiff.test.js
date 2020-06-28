36f3b5b7950d584079e70612620acacf7f03d3f6
Alikhan Askarov <aaskarov@kaptechnology.kazatomprom.kz>
2020-06-28 21:46:45 +0600

    fix step 3, make correct indentation

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,5 +8,7 @@ test('test', () => {
   const before = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/before.json'), 'utf-8');
   const after = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/after.json'), 'utf-8');
   const res = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/result.txt'), 'utf-8');
+  console.log(res);
+  console.log(gendiff(before, after));
   expect(gendiff(before, after)).toBe(res);
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -8,14 +8,14 @@ export default (fileA, fileB) => {
   const fileAKeys = Object.keys(fileAJson);
   const fileBKeys = Object.keys(fileBJson);
 
-  const removedAcc = fileAKeys.reduce((acc, key) => (_.has(fileBJson, key) ? acc : [...acc, `- ${key}: ${fileAJson[key]}`]), []);
-  const addedAcc = fileBKeys.reduce((acc, key) => (_.has(fileAJson, key) ? acc : [...acc, `+ ${key}: ${fileBJson[key]}`]), []);
+  const removedAcc = fileAKeys.reduce((acc, key) => (_.has(fileBJson, key) ? acc : [...acc, ` - ${key}: ${fileAJson[key]}\n`]), []);
+  const addedAcc = fileBKeys.reduce((acc, key) => (_.has(fileAJson, key) ? acc : [...acc, ` + ${key}: ${fileBJson[key]}\n`]), []);
   const changedAcc = fileAKeys.reduce((acc, key) => {
     if (_.has(fileBJson, key) && fileAJson[key] !== fileBJson[key]) {
-      return [...acc, `+ ${key}: ${fileAJson[key]}`, `- ${key}: ${fileBJson[key]}`];
+      return [...acc, ` - ${key}: ${fileAJson[key]}\n`, ` + ${key}: ${fileBJson[key]}\n`];
     }
     if (_.has(fileBJson, key) && fileAJson[key] === fileBJson[key]) {
-      return [...acc, `${key}: ${fileAJson[key]}`];
+      return [...acc, `   ${key}: ${fileAJson[key]}\n`];
     }
     return acc;
   }, []);
@@ -23,6 +23,6 @@ export default (fileA, fileB) => {
   const removedString = prettifyResultList(removedAcc);
   const addedString = prettifyResultList(addedAcc);
   const changedString = prettifyResultList(changedAcc);
-  const resultString = `{ ${[...changedString]} ${[...removedString]} ${[...addedString]} }`.split(', ').join(' ');
+  const resultString = `{\n ${[...changedString]} ${[...removedString]} ${[...addedString]}}\n`.split(', ').join(' ');
   return resultString;
 };
