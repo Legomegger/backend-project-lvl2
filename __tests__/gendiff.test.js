29a057e6581ccd8a917bd02f772b42d8921b102d
Alikhan Askarov <aaskarov@kaptechnology.kazatomprom.kz>
2020-06-29 18:52:46 +0600

    change test name

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../index.js';
 
-test('test', () => {
+test('Testing plain jsons', () => {
   const dirname = path.resolve();
   const before = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/before.json'), 'utf-8');
   const after = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/after.json'), 'utf-8');
