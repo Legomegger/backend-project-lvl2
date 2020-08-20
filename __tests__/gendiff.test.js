df6ab1486eca2d3e771c0f28b266607f9ced9517
Legomegger <legomegakst@gmail.com>
2020-08-20 23:49:57 +0600

    add trim to result fixture

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,7 +7,7 @@ test('Testing plain jsons', () => {
   const dirname = path.resolve();
   const before = path.join(dirname, './__tests__/__fixtures__/before.json');
   const after = path.join(dirname, './__tests__/__fixtures__/after.json');
-  const res = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/result.txt'), 'utf-8');
+  const res = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/result.txt'), 'utf-8').trim();
   console.log(gendiff(before, after));
   expect(gendiff(before, after)).toBe(res);
 });
@@ -16,7 +16,7 @@ test('Testing plain ymls', () => {
   const dirname = path.resolve();
   const before = path.join(dirname, './__tests__/__fixtures__/before.yml');
   const after = path.join(dirname, './__tests__/__fixtures__/after.yml');
-  const res = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/result.txt'), 'utf-8');
+  const res = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/result.txt'), 'utf-8').trim();
   console.log(gendiff(before, after));
   expect(gendiff(before, after)).toBe(res);
 });
@@ -25,7 +25,7 @@ test('Testing plain inis', () => {
   const dirname = path.resolve();
   const before = path.join(dirname, './__tests__/__fixtures__/before.ini');
   const after = path.join(dirname, './__tests__/__fixtures__/after.ini');
-  const res = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/result.txt'), 'utf-8');
+  const res = fs.readFileSync(path.join(dirname, './__tests__/__fixtures__/result.txt'), 'utf-8').trim();
   console.log(gendiff(before, after));
   expect(gendiff(before, after)).toBe(res);
 });
