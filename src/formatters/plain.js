5cc9bd76d01fa53126a1c5a3bfffc657b0d7af61
Alikhan Askarov <aaskarov@kaptechnology.kazatomprom.kz>
2020-09-01 13:18:02 +0600

    WIP

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,51 +8,31 @@ const printValue = (value) => {
 };
 
 const plain = (diff) => {
-  const iter = (node, ancestors) => node.reduce((acc, e) => {
-    if (e.type === 'removed') {
-      if (ancestors.length === 0) {
-        return acc.concat(`Property '${e.key}' was removed\n`);
-      }
-      return acc.concat(`Property '${ancestors.join('.')}.${e.key}' was removed\n`);
+  const iter = (diffObject, ancestors) => diffObject.reduce((acc, node) => {
+    const keys = [...ancestors, node.key];
+    if (node.type === 'removed') {
+      return acc.concat(`Property '${keys.join('.')}' was removed\n`);
     }
-    if (e.type === 'added') {
-      if (ancestors.length === 0) {
-        if (_.isObject(e.value)) {
-          return acc.concat(`Property '${e.key}' was added with value: [complex value]\n`);
-        }
-        return acc.concat(`Property '${e.key}' was added with value: ${printValue(e.value)}\n`);
+    if (node.type === 'added') {
+      if (_.isObject(node.value)) {
+        return acc.concat(`Property '${keys.join('.')}' was added with value: [complex value]\n`);
       }
-
-      if (_.isObject(e.value)) {
-        return acc.concat(`Property '${ancestors.join('.')}.${e.key}' was added with value: [complex value]\n`);
-      }
-      return acc.concat(`Property '${ancestors.join('.')}.${e.key}' was added with value: ${printValue(e.value)}\n`);
+      return acc.concat(`Property '${keys.join('.')}' was added with value: ${printValue(node.value)}\n`);
     }
 
-    if (e.type === 'changed') {
-      if (ancestors.length === 0) {
-        if (_.isObject(e.beforeValue)) {
-          return acc.concat(`Property '${e.key}' was updated. From [complex value] to ${printValue(e.afterValue)}\n`);
-        }
-        if (_.isObject(e.afterValue)) {
-          return acc.concat(`Property '${e.key}' was updated. From ${printValue(e.beforeValue)} to [complex value]\n`);
-        }
-        if (!_.isObject(e.beforeValue) && !_.isObject(e.afterValue)) {
-          return acc.concat(`Property '${e.key}' was updated. From ${printValue(e.beforeValue)} to ${printValue(e.afterValue)}\n`);
-        }
+    if (node.type === 'changed') {
+      if (_.isObject(node.beforeValue)) {
+        return acc.concat(`Property '${keys.join('.')}' was updated. From [complex value] to ${printValue(node.afterValue)}\n`);
       }
-      if (_.isObject(e.beforeValue)) {
-        return acc.concat(`Property '${ancestors.join('.')}.${e.key}' was updated. From [complex value] to ${printValue(e.afterValue)}\n`);
+      if (_.isObject(node.afterValue)) {
+        return acc.concat(`Property '${keys.join('.')}' was updated. From ${printValue(node.beforeValue)} to [complex value]\n`);
       }
-      if (_.isObject(e.afterValue)) {
-        return acc.concat(`Property '${ancestors.join('.')}.${e.key}' was updated. From ${printValue(e.beforeValue)} to [complex value]\n`);
-      }
-      if (!_.isObject(e.beforeValue) && !_.isObject(e.afterValue)) {
-        return acc.concat(`Property '${ancestors.join('.')}.${e.key}' was updated. From ${printValue(e.beforeValue)} to ${printValue(e.afterValue)}\n`);
+      if (!_.isObject(node.beforeValue) && !_.isObject(node.afterValue)) {
+        return acc.concat(`Property '${keys.join('.')}' was updated. From ${printValue(node.beforeValue)} to ${printValue(node.afterValue)}\n`);
       }
     }
-    if (e.type === 'nested') {
-      return acc.concat(iter(e.children[0], [...ancestors, e.key]));
+    if (node.type === 'nested') {
+      return acc.concat(iter(node.children[0], [...ancestors, node.key]));
     }
     return acc;
   }, '');
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,55 +1,56 @@
 import _ from 'lodash';
 
-const printObject = (object, indentation = 2, spacer = ' ') => Object.entries(object).reduce((acc, [key, value]) => {
-  if (_.isObject(value)) {
-    return acc.concat(`${spacer.repeat(indentation + 2)}${key}: {\n${printObject(value, indentation + 4)}${spacer.repeat(indentation + 2)}}\n`);
-  }
-  return acc.concat(`${spacer.repeat(indentation + 2)}${key}: ${value}\n`);
-}, '');
+const space = ' ';
+
+const printObject = (object, indentation = 2, spacer = ' ') => {
+  const entries = Object.entries(object);
+  return entries.reduce((acc, [key, value]) => {
+    if (_.isObject(value)) {
+      return acc.concat(`${spacer.repeat(indentation + 2)}${key}: {\n${printObject(value, indentation + 4)}${spacer.repeat(indentation + 2)}}\n`);
+    }
+    return acc.concat(`${spacer.repeat(indentation + 2)}${key}: ${value}\n`);
+  }, '');
+};
 
 const stylish = (diff, ind) => {
-  const indent = () => {
-    const space = ' ';
-    return space.repeat(ind);
-  };
   const result = diff.reduce((acc, e) => {
     if (e.type === 'removed') {
       if (_.isObject(e.value)) {
-        return acc.concat(`${indent()}- ${e.key}: {\n${printObject(e.value, ind + 4)}${indent()}  }\n`);
+        return acc.concat(`${space.repeat(ind)}- ${e.key}: {\n${printObject(e.value, ind + 4)}${space.repeat(ind)}  }\n`);
       }
-      return acc.concat(`${indent()}- ${e.key}: ${e.value}\n`);
+      return acc.concat(`${space.repeat(ind)}- ${e.key}: ${e.value}\n`);
     }
     if (e.type === 'added') {
       if (_.isObject(e.value)) {
-        return acc.concat(`${indent()}+ ${e.key}: {\n${printObject(e.value, ind + 4)}${indent()}  }\n`);
+        return acc.concat(`${space.repeat(ind)}+ ${e.key}: {\n${printObject(e.value, ind + 4)}${space.repeat(ind)}  }\n`);
       }
-      return acc.concat(`${indent()}+ ${e.key}: ${e.value}\n`);
+      return acc.concat(`${space.repeat(ind)}+ ${e.key}: ${e.value}\n`);
     }
     if (e.type === 'unchanged') {
       if (_.isObject(e.value)) {
-        return acc.concat(`${indent()}  ${e.key}: {\n ${printObject(e.value, ind + 4)}}\n`);
+        return acc.concat(`${space.repeat(ind)}  ${e.key}: {\n ${printObject(e.value, ind + 4)}}\n`);
       }
-      return acc.concat(`${indent()}  ${e.key}: ${e.value}\n`);
+      return acc.concat(`${space.repeat(ind)}  ${e.key}: ${e.value}\n`);
     }
     if (e.type === 'changed') {
       if (_.isObject(e.beforeValue) && _.isObject(e.afterValue)) {
-        return acc.concat(`${indent()}- ${e.key}: {\n${printObject(e.beforeValue, ind + 4)}}\n${indent()}+ ${e.key}: {\n${printObject(e.afterValue, ind + 4)}}\n`);
+        return acc.concat(`${space.repeat(ind)}- ${e.key}: {\n${printObject(e.beforeValue, ind + 4)}}\n${space.repeat(ind)}+ ${e.key}: {\n${printObject(e.afterValue, ind + 4)}}\n`);
       }
 
       if (!_.isObject(e.beforeValue) && !_.isObject(e.afterValue)) {
-        return acc.concat(`${indent()}- ${e.key}: ${e.beforeValue}\n${indent()}+ ${e.key}: ${e.afterValue}\n`);
+        return acc.concat(`${space.repeat(ind)}- ${e.key}: ${e.beforeValue}\n${space.repeat(ind)}+ ${e.key}: ${e.afterValue}\n`);
       }
 
       if (!_.isObject(e.beforeValue) && _.isObject(e.afterValue)) {
-        return acc.concat(`${indent()}- ${e.key}: ${e.beforeValue}\n${indent()}+ ${e.key}: {\n${printObject(e.afterValue, ind + 4)}${indent()}  }\n`);
+        return acc.concat(`${space.repeat(ind)}- ${e.key}: ${e.beforeValue}\n${space.repeat(ind)}+ ${e.key}: {\n${printObject(e.afterValue, ind + 4)}${space.repeat(ind)}  }\n`);
       }
 
       if (_.isObject(e.beforeValue) && !_.isObject(e.afterValue)) {
-        return acc.concat(`${indent()}- ${e.key}: {\n${printObject(e.beforeValue, ind + 4)}${indent()}  }\n${indent()}+ ${e.key}: ${e.afterValue}\n`);
+        return acc.concat(`${space.repeat(ind)}- ${e.key}: {\n${printObject(e.beforeValue, ind + 4)}${space.repeat(ind)}  }\n${space.repeat(ind)}+ ${e.key}: ${e.afterValue}\n`);
       }
     }
     if (e.type === 'nested') {
-      return acc.concat(`${indent()}  ${e.key}: {\n${stylish(e.children[0], ind + 4)}${indent()}  }\n`);
+      return acc.concat(`${space.repeat(ind)}  ${e.key}: {\n${stylish(e.children[0], ind + 4)}${space.repeat(ind)}  }\n`);
     }
     return acc;
   }, '');
