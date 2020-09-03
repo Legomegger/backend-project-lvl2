9c129ae4ac4e20a365359627869a2e713ffbc405
Alikhan Askarov <aaskarov@kaptechnology.kazatomprom.kz>
2020-09-03 14:11:44 +0600

    refactor stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,15 +6,15 @@ const printObject = (object, indentation = 2) => {
   const entries = Object.entries(object);
   return entries.reduce((acc, [key, value]) => {
     if (_.isObject(value)) {
-      return acc.concat(`${space.repeat(indentation + 2)}${key}: {\n${printObject(value, indentation + 8)}${space.repeat(indentation + 4)}}\n`);
+      return acc.concat(`${space.repeat(indentation + 6)}${key}: {\n${printObject(value, indentation + 4)}${space.repeat(indentation + 6)}}\n`);
     }
-    return acc.concat(`${space.repeat(indentation + 2)}${key}: ${value}\n`);
+    return acc.concat(`${space.repeat(indentation + 6)}${key}: ${value}\n`);
   }, '');
 };
 
 const renderValue = (value, indentation) => {
   if (_.isObject(value)) {
-    return `{\n${space.repeat(indentation + 2)}${printObject(value, indentation)}${space.repeat(indentation + 2)} }`;
+    return `{\n${printObject(value, indentation)}${space.repeat(indentation + 2)}}`;
   }
   return value;
 };
@@ -22,22 +22,21 @@ const renderValue = (value, indentation) => {
 const stylish = (diff) => {
   const iter = (diffObject, indentation) => diffObject.map((node) => {
     if (node.type === 'removed') {
-      return `${space.repeat(indentation)} - ${node.key}: ${renderValue(node.value, indentation)}`;
+      return `${space.repeat(indentation)}- ${node.key}: ${renderValue(node.value, indentation)}`;
     }
     if (node.type === 'added') {
-      return `${space.repeat(indentation)} + ${node.key}: ${renderValue(node.value, indentation)}`;
+      return `${space.repeat(indentation)}+ ${node.key}: ${renderValue(node.value, indentation)}`;
     }
     if (node.type === 'unchanged') {
       return `${space.repeat(indentation)}  ${node.key}: ${renderValue(node.value, indentation)}`;
     }
     if (node.type === 'changed') {
-      return `${space.repeat(indentation)} - ${node.key}: ${renderValue(node.beforeValue, indentation)}\n ${space.repeat(indentation)} + ${node.key}: ${renderValue(node.afterValue, indentation)}`;
+      return `${space.repeat(indentation)}- ${node.key}: ${renderValue(node.beforeValue, indentation)}\n${space.repeat(indentation)}+ ${node.key}: ${renderValue(node.afterValue, indentation)}`;
     }
-    return `${space.repeat(indentation)}   ${node.key}: {\n ${iter(node.children, indentation + 2).join('\n')}${space.repeat(indentation + 2)}\n}`;
+    return `${space.repeat(indentation)}  ${node.key}: {\n${iter(node.children, indentation + 4).join('\n')}\n${space.repeat(indentation + 2)}}`;
   });
-  return iter(diff, 2);
+  const result = iter(diff, 2);
+  return `{\n${result.join('\n')}\n}`;
 };
 
-const wrapResult = (diff) => console.log(stylish(diff)[0]);
-
-export default wrapResult;
+export default stylish;
