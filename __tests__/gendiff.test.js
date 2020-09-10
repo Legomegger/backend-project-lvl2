4c739ffd585f0328f145ca8881cab133400b6b16
Alikhan Askarov <aaskarov@kaptechnology.kazatomprom.kz>
2020-09-10 14:44:29 +0600

    refactor stylish, test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,21 +11,20 @@ let resultJson;
 
 const extensions = ['yml', 'ini', 'json'];
 
-const getFixturePath = (filename) => {
+const getFixturePath = (filename, extension) => {
   const dirname = path.resolve();
-  return path.join(dirname, '.', '__tests__', '__fixtures__', filename);
+  return path.join(dirname, '.', '__tests__', '__fixtures__', `${filename}.${extension}`);
 };
 
 beforeAll(() => {
-  resultStylish = fs.readFileSync(getFixturePath('stylish.txt'), 'utf-8').trim();
-  resultPlain = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8').trim();
-  resultJson = fs.readFileSync(getFixturePath('json.txt'), 'utf-8').trim();
+  resultStylish = fs.readFileSync(getFixturePath('stylish', 'txt'), 'utf-8').trim();
+  resultPlain = fs.readFileSync(getFixturePath('plain', 'txt'), 'utf-8').trim();
+  resultJson = fs.readFileSync(getFixturePath('json', 'txt'), 'utf-8').trim();
 });
 
 extensions.forEach((extension) => {
-  const dirname = path.resolve();
-  const before = path.join(dirname, `./__tests__/__fixtures__/before.${extension}`);
-  const after = path.join(dirname, `./__tests__/__fixtures__/after.${extension}`);
+  const before = getFixturePath('before', extension);
+  const after = getFixturePath('after', extension);
   describe('test stylish', () => {
     test(`${extension} stylish`, () => {
       expect(gendiff(before, after, 'stylish')).toBe(resultStylish);
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,33 +2,33 @@ import _ from 'lodash';
 
 const space = ' ';
 
-const getIndent = (depth, by = 0) => depth + by;
+const getIndent = (depth) => space.repeat(depth);
 
-const renderValue = (value, indentation) => {
+const renderValue = (value, depth) => {
   if (!_.isObject(value)) {
     return value;
   }
   const entries = Object.entries(value);
   const mapped = entries.map(([key, val]) => {
-    const line = `${space.repeat(indentation + 6)}${key}: ${renderValue(val, indentation + 4)}`;
+    const line = `${space.repeat(depth + 6)}${key}: ${renderValue(val, depth + 4)}`;
     return line;
   });
-  return `{\n${mapped.join('\n')}\n${space.repeat(indentation + 2)}}`;
+  return `{\n${mapped.join('\n')}\n${space.repeat(depth + 2)}}`;
 };
 
 const stylish = (diff) => {
   const iter = (diffObject, depth) => diffObject.map((node) => {
     switch (node.type) {
       case 'removed':
-        return `${space.repeat(getIndent(depth))}- ${node.key}: ${renderValue(node.value, getIndent(depth))}`;
+        return `${getIndent(depth)}- ${node.key}: ${renderValue(node.value, depth)}`;
       case 'added':
-        return `${space.repeat(getIndent(depth))}+ ${node.key}: ${renderValue(node.value, getIndent(depth))}`;
+        return `${getIndent(depth)}+ ${node.key}: ${renderValue(node.value, depth)}`;
       case 'unchanged':
-        return `${space.repeat(getIndent(depth))}  ${node.key}: ${renderValue(node.value, getIndent(depth))}`;
+        return `${getIndent(depth)}  ${node.key}: ${renderValue(node.value, depth)}`;
       case 'changed':
-        return `${space.repeat(getIndent(depth))}- ${node.key}: ${renderValue(node.beforeValue, getIndent(depth))}\n${space.repeat(getIndent(depth))}+ ${node.key}: ${renderValue(node.afterValue, getIndent(depth))}`;
+        return `${getIndent(depth)}- ${node.key}: ${renderValue(node.beforeValue, depth)}\n${getIndent(depth)}+ ${node.key}: ${renderValue(node.afterValue, depth)}`;
       default:
-        return `${space.repeat(getIndent(depth))}  ${node.key}: {\n${iter(node.children, depth + 4).join('\n')}\n${space.repeat(getIndent(depth + 2))}}`;
+        return `${getIndent(depth)}  ${node.key}: {\n${iter(node.children, depth + 4).join('\n')}\n${getIndent(depth + 2)}}`;
     }
   });
   return `{\n${iter(diff, 2).join('\n')}\n}`;
